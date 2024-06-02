adaed8bb8f9b0eef1339bec7252a419bb2cdad11
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2024-06-02 16:39:19 +0300

    update

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/policy.html',
+      filename: 'policy.html',
+      inject: 'body',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/policy.html/, to: '/policy.html' },
+      ],
+    },
+  },
+  mode: 'development',
+};
